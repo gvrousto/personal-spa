67224b649dae5768616044de87a435ffab86844a
Gus Vroustouris <gvroustouris@gmail.com>
2020-08-16 18:03:27 -0500

    about now cmpleted

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,83 +14,85 @@ const About = () => {
           {paragraph}
         </div>
       </div>
-      <div className="about-experience">
-        <div className="experience-title">
-          Experience
-        </div>
-        <div className="experience-content">
-          <div className="experience-bubble">
-            <div className="bubble-text">
-              Discover
+      <div className="helper-container">
+        <div className="about-experience">
+          <div className="experience-title">
+            Experience
+          </div>
+          <div className="experience-content">
+            <div className="experience-bubble">
+              <div className="bubble-text">
+                Discover
+              </div>
+            </div>
+            <div className="time-range">
+              June 2019 - Present
+            </div>
+            <div className="job-title">
+              Software Engineer
+            </div>
+            <div className="time-range">
+              May 2018 - Aug 2018
+            </div>
+            <div className="job-title">
+              Software Engineer Intern
             </div>
           </div>
-          <div className="time-range">
-            June 2019 - Present
-          </div>
-          <div className="job-title">
-            Software Engineer
-          </div>
-          <div className="time-range">
-            May 2018 - Aug 2018
-          </div>
-          <div className="job-title">
-            Software Engineer Intern
-          </div>
-        </div>
-        <div className="experience-content">
-          <div className="experience-bubble">
-            <div className="bubble-text">
-              Oqullo
+          <div className="experience-content">
+            <div className="experience-bubble">
+              <div className="bubble-text">
+                Oqullo
+              </div>
+            </div>
+            <div className="time-range">
+              Feb 2020 - May 2020
+            </div>
+            <div className="job-title">
+              Software Engineer Contractor
             </div>
           </div>
-          <div className="time-range">
-            Feb 2020 - May 2020
-          </div>
-          <div className="job-title">
-            Software Engineer Contractor
-          </div>
-        </div>
-        <div className="experience-content">
-          <div className="experience-bubble">
-            <div className="bubble-text">
-              Neighborhoods
+          <div className="experience-content">
+            <div className="experience-bubble">
+              <div className="bubble-text">
+                Neighborhoods
+              </div>
+            </div>
+            <div className="time-range">
+              June 2017 - Aug 2017
+            </div>
+            <div className="job-title">
+              Software Engineer Intern
             </div>
           </div>
-          <div className="time-range">
-            June 2017 - Aug 2017
-          </div>
-          <div className="job-title">
-            Software Engineer Intern
-          </div>
-        </div>
-        <div className="experience-content">
-          <div className="experience-bubble">
-            <div className="bubble-text">
-              DoIT
+          <div className="experience-content">
+            <div className="experience-bubble">
+              <div className="bubble-text">
+                DoIT
+              </div>
+            </div>
+            <div className="time-range">
+              July 2014 - Sept 2014
+            </div>
+            <div className="job-title">
+              Technology Intern
             </div>
           </div>
-          <div className="time-range">
-            July 2014 - Sept 2014
-          </div>
-          <div className="job-title">
-            Technology Intern
-          </div>
         </div>
-      </div>
-      <div className="about-skills">
-        <div className="skills-title">Skills</div>
-          <div className="skill-list">
-            <div className="skill-item">Java Spring Boot</div>
-            <div className="skill-item">Cloud Computing</div>
-            <div className="skill-item">Typescript & React</div>
-            <div className="skill-item">Jenkins (CI/CD automation)</div>
-            <div className="skill-item">Cucumber Test Scripts</div>
-            <div className="skill-item">User Story Estimation and Distribution</div>
-            <div className="skill-item">Public Speaking and Presentations</div>
-            <div className="skill-item">NodeJS</div>
-            <div className="skill-item">Databases (MongoDB/Redis/MySQL)</div>
-            <div className="skill-item">Making Friends</div>
-          </div>
+        <div className="about-skills">
+          <div className="skills-title">Skills</div>
+            <div className="skill-list">
+              <div className="skill-item">Java Spring Boot</div>
+              <div className="skill-item">Cloud Computing</div>
+              <div className="skill-item">Typescript & React</div>
+              <div className="skill-item">Jenkins (CI/CD automation)</div>
+              <div className="skill-item">Cucumber Test Scripts</div>
+              <div className="skill-item">User Story Estimation and Distribution</div>
+              <div className="skill-item">Public Speaking and Presentations</div>
+              <div className="skill-item">NodeJS</div>
+              <div className="skill-item">Databases (MongoDB / Redis / MySQL)</div>
+              <div className="skill-item">Making Friends</div>
+            </div>
+        </div>
       </div>
     </div>
   );
